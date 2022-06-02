d9939a35b6f72cd93faad15ad7d7463e8e14bacf
abanoubmesiha <anagy@app-corp.com>
2022-06-02 17:59:11 +0200

    Add api for getting all sections.

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import sections
 
 urlpatterns = [
-    path('sections/get_all', sections.get_all, name="get_all_sections"),
+    path('sections/get_all', sections.get_all),
 ]
--- core/views/sections.py
+++ core/views/sections.py
@@ -1,5 +1,9 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
+
+from .utils import envelope
 from ..models import Section
 
-def get_all(request):
-    return HttpResponse('hi')
+def get_all():
+    sections = Section.objects.all()
+    data = [section.serialize() for section in sections]
+    return JsonResponse(envelope(data))
