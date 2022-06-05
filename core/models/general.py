8c548990ee98cc22e8f36a71d4d6dee31704a618
abanoubmesiha <anagy@app-corp.com>
2022-06-05 15:31:03 +0200

    - Create model Artist - Create model Genre

--- core/models/general.py
+++ core/models/general.py
@@ -46,3 +46,40 @@ class Section(models.Model):
             "series": [try_to_serialize(series) for series in self.series.all()],
         }
 
+class Genre(models.Model):
+    title_ar = models.CharField(max_length=150)
+    title_en = models.CharField(max_length=150)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title_en}"
+
+    def serialize(self, options={}):
+        return {
+            "id": self.id,
+
+            "title_ar": self.title_ar,
+            "title_en": self.title_en,
+
+            "created_at": self.created_at,
+        }
+
+class Artist(models.Model):
+    title_ar = models.CharField(max_length=150)
+    title_en = models.CharField(max_length=150)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title_en}"
+
+    def serialize(self, options={}):
+        return {
+            "id": self.id,
+
+            "title_ar": self.title_ar,
+            "title_en": self.title_en,
+
+            "created_at": self.created_at,
+        }
