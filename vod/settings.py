d012922dc60ddc8a91ec2e8f053f99d6a3f2b83a
abanoubmesiha <anagy@app-corp.com>
2022-06-06 20:00:15 +0200

    fix db base ur;

--- vod/settings.py
+++ vod/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'vod.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR.as_posix() / 'db.sqlite3',
         # 'NAME': BASE_DIR.as_posix() +'db.sqlite3',
     }
 }
