694b6a6a6c55ee8ef5c94406f8194674aea370ae
abanoubmesiha <anagy@app-corp.com>
2022-06-06 20:11:28 +0200

    Fix static url

--- vod/settings.py
+++ vod/settings.py
@@ -117,11 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = BASE_DIR.as_posix() + '/static'
+STATIC_URL = BASE_DIR.as_posix() + '/static'
 # MEDIA_ROOT = BASE_DIR.as_posix()+ "media"
