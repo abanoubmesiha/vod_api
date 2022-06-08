c9212fa8e8c2dbb76277d9857ae85ca2b8c7d1fd
abanoubmesiha <anagy@app-corp.com>
2022-06-08 18:38:50 +0200

    Add rest framework with token authentication.

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 from .views import sections
 from .views import movies
 from .views import series
+from .views import auth
 
 urlpatterns = [
+    path('login/', auth.login),
+    path('api-token-auth/', obtain_auth_token),
     path('sections', sections.get_all),
     path('movies/<int:movie_id>', movies.get_one),
     path('series/<int:series_id>', series.get_one),
