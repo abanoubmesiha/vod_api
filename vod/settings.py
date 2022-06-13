ee446dec78056d5e627ad6aa416b17035c3d039b
abanoubmesiha <anagy@app-corp.com>
2022-06-13 18:48:28 +0200

    Use astrisk for allowed hosts and origins.

--- vod/settings.py
+++ vod/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-hn6pv8&8pl$t8pkw&h(*i-c7rx_zsk&86e$sf9!6+aj2gp#$$!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '49.12.195.122']
-CORS_ALLOWED_ORIGINS = ['http://localhost', 'http://127.0.0.1', 'http://49.12.195.122', 'http://49.12.195.122:8000']
+ALLOWED_HOSTS = ["*"]
+CORS_ALLOWED_ORIGINS = ["*"]
 CORS_ALLOW_HEADERS = [
     "accept",
     "accept-encoding",
