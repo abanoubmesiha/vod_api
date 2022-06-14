46f75a7ad0a490b17dcd41baab7273ec2e3e8246
abanoubmesiha <anagy@app-corp.com>
2022-06-14 16:32:20 +0200

    Lowercase some headers.

--- core/views/sections.py
+++ core/views/sections.py
@@ -14,8 +14,8 @@ def test_cors(request):
     sections = Section.objects.all()
     data = [section.serialize() for section in sections]
     response = JsonResponse(envelope(data))
-    response["Access-Control-Allow-Origin"] = 'http://49.12.195.122:3000/'
-    response['Access-Control-Allow-Methods'] = '*'
-    response['Access-Control-Allow-Headers'] = 'Content-Type'
+    response["access-control-allow-origin"] = 'http://49.12.195.122:3000'
+    response['access-control-allow-methods'] = 'GET, PUT, POST, DELETE, HEAD, OPTIONS'
+    # response['access-control-allow-headers'] = 'Content-Type'
     response['Content-Type'] = 'application/json'
     return response
