155960d440468f8a2506f5e1120b9a78454817fb
abanoubmesiha <anagy@app-corp.com>
2022-06-15 13:15:44 +0200

    Add cors header for a test.

--- core/views/auth.py
+++ core/views/auth.py
@@ -7,3 +7,11 @@ from rest_framework.permissions import IsAuthenticated
 def login(request):
     content = {'message': 'Hello, World!'}
     return Response(content)
+
+def get_token(request):
+    response = Response('59475edec9930601664b5fa6a13b2464cc05130d')
+    response["access-control-allow-origin"] = 'http://49.12.195.122:8000'
+    response['access-control-allow-methods'] = 'GET, PUT, POST, DELETE, HEAD, OPTIONS'
+    response['access-control-allow-headers'] = 'Content-Type, Authorization'
+    response['Content-Type'] = 'application/json'
+    return response
