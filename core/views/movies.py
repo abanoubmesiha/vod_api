cbadea2d559c252fc7fc171c0cbd308ded437652
abanoubmesiha <anagy@app-corp.com>
2022-06-15 14:43:18 +0200

    Add try statement for movie cast.

--- core/views/movies.py
+++ core/views/movies.py
@@ -12,7 +12,11 @@ def get_one(request, movie_id):
 
         serialized_movie = movie.serialize()
         serialized_movie['comments'] = [comment.serialize() for comment in comments]
-        serialized_movie['cast'] = movie.cast.serialize()
+        try:
+            serialized_movie['cast'] = movie.cast.serialize()
+        except:
+            serialized_movie['cast'] = None
+            
         return JsonResponse(envelope(serialized_movie))
     except Movie.DoesNotExist:
         return JsonResponse(envelope(None, 404, 'Item Not Found'))
