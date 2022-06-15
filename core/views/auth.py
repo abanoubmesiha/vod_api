38cd84abc857e9347f8d1f1fd4a89ae2374ae5f3
abanoubmesiha <anagy@app-corp.com>
2022-06-15 13:35:19 +0200

    Add envlope

--- core/views/auth.py
+++ core/views/auth.py
@@ -3,6 +3,8 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
+from core.views.utils import envelope
+
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def login(request):
@@ -10,7 +12,7 @@ def login(request):
     return Response(content)
 
 def get_token(request):
-    response = JsonResponse('59475edec9930601664b5fa6a13b2464cc05130d')
+    response = JsonResponse(envelope({'token': '59475edec9930601664b5fa6a13b2464cc05130d'}))
     response["access-control-allow-origin"] = 'http://49.12.195.122:8000'
     response['access-control-allow-methods'] = 'GET, PUT, POST, DELETE, HEAD, OPTIONS'
     response['access-control-allow-headers'] = 'Content-Type, Authorization'
