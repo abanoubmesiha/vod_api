311d27bf7a3f6534d8b9d7b4def2c7c9097e3241
abanoubmesiha <anagy@app-corp.com>
2022-06-15 15:06:21 +0200

    Remove Cast Model

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import (
     Movie, Country, Series, Episode, Section, Artist, Actor, Director,
-    Genre, Comment, Cast
+    Genre, Comment
 )
 
 
@@ -15,4 +15,3 @@ admin.site.register(Actor)
 admin.site.register(Director)
 admin.site.register(Genre)
 admin.site.register(Comment)
-admin.site.register(Cast)
--- core/views/movies.py
+++ core/views/movies.py
@@ -12,10 +12,6 @@ def get_one(request, movie_id):
 
         serialized_movie = movie.serialize()
         serialized_movie['comments'] = [comment.serialize() for comment in comments]
-        try:
-            serialized_movie['cast'] = movie.cast.serialize()
-        except:
-            serialized_movie['cast'] = None
             
         return JsonResponse(envelope(serialized_movie))
     except Movie.DoesNotExist:
