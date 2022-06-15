5391a673ec5969bd7476502fa9bdde6528867b72
abanoubmesiha <anagy@app-corp.com>
2022-06-15 13:38:19 +0200

    disable csrf

--- core/views/auth.py
+++ core/views/auth.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
