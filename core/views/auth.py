6c70d45c91be51c6bb4531cd44a118d967f8e9e2
abanoubmesiha <anagy@app-corp.com>
2022-06-15 13:41:54 +0200

    Remove allow origin

--- core/views/auth.py
+++ core/views/auth.py
@@ -15,7 +15,7 @@ def login(request):
 @csrf_exempt
 def get_token(request):
     response = JsonResponse(envelope({'token': '59475edec9930601664b5fa6a13b2464cc05130d'}))
-    response["access-control-allow-origin"] = 'http://49.12.195.122:8000'
+    # response["access-control-allow-origin"] = 'http://49.12.195.122:8000'
     response['access-control-allow-methods'] = 'GET, PUT, POST, DELETE, HEAD, OPTIONS'
     response['access-control-allow-headers'] = 'Content-Type, Authorization'
     response['Content-Type'] = 'application/json'
