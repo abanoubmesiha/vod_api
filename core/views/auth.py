eac0cbcca2e97357b022f85710bae0a4cb4ef3b2
abanoubmesiha <anagy@app-corp.com>
2022-06-15 13:33:17 +0200

    Use json reponse

--- core/views/auth.py
+++ core/views/auth.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
@@ -9,7 +10,7 @@ def login(request):
     return Response(content)
 
 def get_token(request):
-    response = Response('59475edec9930601664b5fa6a13b2464cc05130d')
+    response = JsonResponse('59475edec9930601664b5fa6a13b2464cc05130d')
     response["access-control-allow-origin"] = 'http://49.12.195.122:8000'
     response['access-control-allow-methods'] = 'GET, PUT, POST, DELETE, HEAD, OPTIONS'
     response['access-control-allow-headers'] = 'Content-Type, Authorization'
