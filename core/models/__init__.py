e5425ff33d7e52bcff9393496d00da284fcaef25
abanoubmesiha <anagy@app-corp.com>
2022-10-11 20:22:05 +0200

    Remove streaming related props from series and movies.

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,4 +1,4 @@
-from .general import Country, Section, SectionAdmin, Streaming_Server, Genre, Comment, CommentAdmin
+from .general import Country, Section, SectionAdmin, Genre, Comment, CommentAdmin
 from .artist import Artist, ArtistAdmin, Actor, ActorAdmin, Director, DirectorAdmin
 from .movie import Movie, MovieAdmin
 from .series import Series, SeriesAdmin, Episode
--- core/views/movies.py
+++ core/views/movies.py
@@ -14,7 +14,7 @@ def get_one(request, movie_id):
         serialized_movie['comments'] = [comment.serialize() for comment in comments]
         
         if request.user.is_authenticated is False:
-            serialized_movie['streaming_server'] = serialized_movie['video'] = serialized_movie['video_medium_q'] = serialized_movie['video_low_q'] = serialized_movie['cdn_video'] = '401 Unauthorized'
+            serialized_movie['cdn_video'] = '401 Unauthorized'
             
         return JsonResponse(envelope(serialized_movie))
     except Movie.DoesNotExist:
@@ -26,10 +26,6 @@ def get_movie_video(request, movie_id):
     try:
         movie = Movie.objects.get(pk=movie_id)
         return JsonResponse(envelope({
-            'streaming_server': movie.streaming_server.url if movie.streaming_server is not None else '',
-            'video': movie.video,
-            'video_medium_q': movie.video_medium_q,
-            'video_low_q': movie.video_low_q,
             'cdn_video': movie.cdn_video,
         }))
     except Movie.DoesNotExist:
