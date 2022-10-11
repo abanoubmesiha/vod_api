a6d961fd246b3350274d18f1284818be79f928cd
abanoubmesiha <anagy@app-corp.com>
2022-10-11 20:13:52 +0200

    Start to loop over csv files.

--- core/views/movies.py
+++ core/views/movies.py
@@ -1,6 +1,7 @@
 from django.http import JsonResponse
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
+import csv
 
 from .utils import envelope
 from ..models import Movie, Comment
@@ -34,3 +35,41 @@ def get_movie_video(request, movie_id):
         }))
     except Movie.DoesNotExist:
         return JsonResponse(envelope(None, 404, 'Item Not Found'))
+
+@api_view(['GET'])
+def add_movies_from_csv(request):
+    try:
+        movies1 = open('core/movies1.csv', encoding="utf-8-sig")
+        moviesCdnOutput = open('core/movies-CdnOutput.csv', encoding="utf8")
+        csv_movies_names = csv.reader(movies1)
+        csv_movies_links = csv.reader(moviesCdnOutput)
+
+        for row in csv_movies_names:
+            movie_name = row[0]
+            video_file_name = row[5]
+            
+            merged_data_movie = None
+
+            row2_video_name = None
+            row2_video_id = None
+            # for row2 in csv_movies_links:
+            #     try:
+            #         row2_video_name = row2[1]
+            #         if row2_video_name == video_file_name:
+            #             row2_video_id = row2[3]
+            #     except:
+            #         pass
+            
+            print(row, video_file_name, row2_video_name, row2_video_id)
+
+            # a_cdn_movie = list(movie for movie in csv_movies_links if movie[1] == video_file_name)[0]
+            # print(csv_movies_links)
+
+
+            # target_movie = Movie.objects.get_or_create(title_ar=movie_name)
+            # target_movie.cdn_video = "https://video.bunnycdn.com/play/50219/" + video_id
+
+        
+        return JsonResponse(envelope(None, 200, 'Still Testing...'))
+    except Exception as e:
+        return JsonResponse(envelope(e, 404, 'Error!'))
