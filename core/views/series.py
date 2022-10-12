ea9f44881f5077e3477c23ebb7f58e236f747520
AshourDono <eng.ahmed.hamdy.ashour@gmail.com>
2022-10-12 14:22:40 +0200

    add series functionality done

--- core/views/series.py
+++ core/views/series.py
@@ -23,7 +23,7 @@ def get_one(request, series_id):
 
 
 # @api_view(['GET'])
-def add_series_episodes(request, pk):
+def add_series_episodes(pk):
     try:
         series_episodes_data = []
         series_title = Series.objects.filter(
@@ -37,45 +37,45 @@ def add_series_episodes(request, pk):
                 series_episodes_data.append(row[1:])
 
         for csv_episode in series_episodes_data:
-            print(csv_episode[0], re.match(
-                "^EPS \d{2}_1_1\.mp4$", csv_episode[0]))
-            if re.match("A[0-9]+\.mp4$", csv_episode[0]):
-                episode_number = csv_episode[0].split(".")[0][-2:]
+            if re.fullmatch("A[0-9]+\.mp4$", csv_episode[0], flags=re.X):
+                episode_number = int(csv_episode[0].split(".")[0][-2:])
 
-            elif re.match("EPS\d{2}_Master\.mp4$", csv_episode[0]):
-                episode_number = csv_episode[0].split("_")[0][-2:]
+            elif re.fullmatch("EPS\d{2}_Master\.mp4$", csv_episode[0]):
+                episode_number = int(csv_episode[0].split("_")[0][-2:])
 
-            elif re.match("[a-zA-Z_0-9-]+_Eps\d{2}\.mp4$", csv_episode[0]):
-                episode_number = csv_episode[0].split("_")[-1].split(".")[
-                    0][-2:]
+            elif re.fullmatch("[a-zA-Z_0-9-]+_Eps\d{2}\.mp4$", csv_episode[0]):
+                episode_number = int(csv_episode[0].split("_")[-1].split(".")[
+                    0][-2:])
 
             # EPS 28_1_1.mp4
-            elif re.match("EPS \d{2}_1_1\.mp4$", csv_episode[0]):
-                episode_number = csv_episode[0].split(" ")[1][:2]
+            elif re.fullmatch("EPS \d{2}_1_1\.mp4$", csv_episode[0]):
+                episode_number = int(csv_episode[0].split(" ")[1][:2])
 
             # EPS07_1_1.mp4
-            elif re.match("EPS\d{2}_1_1\.mp4$", csv_episode[0]):
-                episode_number = csv_episode[0].split("_")[0][-2:]
+            elif re.fullmatch("EPS\d{2}_1_1\.mp4$", csv_episode[0]):
+                episode_number = int(csv_episode[0].split("_")[0][-2:])
 
-            elif re.match("[a-zA-Z0-9 ]+EPS\d{2}\.mp4$", csv_episode[0]):
-                episode_number = csv_episode[0].split(" ")[-1].split(".")[
-                    0][-2:]
+            elif re.fullmatch("[a-zA-Z0-9 ]*EPS\d{2}\.mp4$", csv_episode[0], flags=re.I):
+                episode_number = int(csv_episode[0].split(" ")[-1].split(".")[
+                    0][-2:])
 
-            elif re.match("EPS-\d{2,3}_1_1\.mp4$", csv_episode[0]):
-                episode_number = csv_episode[0].split("-")[
-                    1].split("_")[0]
-                    
-            elif re.match("[0-9]+\.mp4$", csv_episode[0]):
-                episode_number = csv_episode[0].split(".")[
-                    0][-2:]
+            elif re.fullmatch("EPS-\d{2,3}_1_1\.mp4$", csv_episode[0]):
+                episode_number = int(csv_episode[0].split("-")[
+                    1].split("_")[0])
+
+            elif re.fullmatch("EPS \d{2}\.mp4$", csv_episode[0], re.IGNORECASE):
+                episode_number = int(csv_episode[0].split(" ")[1][:2])
+
+            elif re.fullmatch("[0-9]+\.mp4$", csv_episode[0]):
+                episode_number = int(csv_episode[0].split(".")[
+                    0][-2:])
 
             video_id = csv_episode[2]
             video_id_plus_thumbnail = csv_episode[3]
 
-            cdn_video_link = "https://video.bunnycdn.com/play/50219/" + video_id
+            cdn_video_link = "https://iframe.mediadelivery.net/embed/50219/" + video_id
             thumbnail_link = "https://vz-76b7c0fe-9e0.b-cdn.net/" + video_id_plus_thumbnail
 
-            print(episode_number)
             try:
                 new_episode = Episode(
                     number=episode_number,
@@ -84,8 +84,8 @@ def add_series_episodes(request, pk):
                     cdn_cover=thumbnail_link
                 )
                 new_episode.save()
-            except:
-                print("fail to save")
+            except Exception as e:
+                return JsonResponse(envelope(e, 400))
 
         series_episodes_data_final_length = Episode.objects.count()
         return JsonResponse(envelope(f"{series_episodes_data_final_length} episodes added", 200, f'{len(series_episodes_data)} episodes of {series_title} series were added/updated successfully'))
@@ -95,6 +95,7 @@ def add_series_episodes(request, pk):
 
 def add_series(request):
     series_titles_array = []
+    series_ids_array = []
     series_titles = Series.objects.values_list('title_en', flat=True)
     series_titles_array = [*series_titles]
 
@@ -112,4 +113,11 @@ def add_series(request):
         else:
             pass
 
-    return JsonResponse(envelope(series_titles_array, 200, f"series added successfully"))
+    series_ids = Series.objects.values_list('pk', flat=True)
+    series_ids_array = [*series_ids]
+
+    for id in series_ids_array:
+        add_series_episodes(id)
+
+    series_episodes_data_final_length = Episode.objects.count()
+    return JsonResponse(envelope(f"{series_episodes_data_final_length} episodes added from {len(series_titles_array)} series", 200))
